e1767c57394783b782a44a3ae49f40f18404c5d7
Michelle Miller <chellimiller@users.noreply.github.com>
2022-02-07 18:15:08 -0700

    #6 Add all routes

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -7,9 +7,38 @@ import withTheme from './ui/withTheme';
 
 const Home = React.lazy(() => import('./pages/Home'));
 const ExamplesHome = React.lazy(() => import('./pages/examples/Home'));
-const ExamplesCandlestickChart = React.lazy(() => import('./pages/examples/CandlestickChart'));
-const ExamplesBarChart = React.lazy(() => import('./pages/examples/BarChart'));
-const ExamplesCustomFontFamily = React.lazy(() => import('./pages/examples/CustomFontFamily'));
+const ExampleBarChart = React.lazy(() => import('./pages/examples/BarChart'));
+const ExampleCandlestickChart = React.lazy(() => import('./pages/examples/CandlestickChart'));
+const ExampleChangeSeriesVisibility = React.lazy(() => import('./pages/examples/ChangeSeriesVisibility'));
+const ExampleCustomFontFamily = React.lazy(() => import('./pages/examples/CustomFontFamily'));
+const ExampleCustomLocale = React.lazy(() => import('./pages/examples/CustomLocale'));
+const ExampleCustomPriceFormatter = React.lazy(() => import('./pages/examples/CustomPriceFormatter'));
+const ExampleCustomThemes = React.lazy(() => import('./pages/examples/CustomThemes'));
+const ExampleCustomWatermark = React.lazy(() => import('./pages/examples/CustomWatermark'));
+const ExampleFitContent = React.lazy(() => import('./pages/examples/FitContent'));
+const ExampleFloatingTooltip = React.lazy(() => import('./pages/examples/FloatingTooltip'));
+const ExampleGoToRealtimeButton = React.lazy(() => import('./pages/examples/GoToRealtimeButton'));
+const ExampleInfiniteHistory = React.lazy(() => import('./pages/examples/InfiniteHistory'));
+const ExampleIntradayData = React.lazy(() => import('./pages/examples/IntradayData'));
+const ExampleInvertedPriceScale = React.lazy(() => import('./pages/examples/InvertedPriceScale'));
+const ExampleLegend = React.lazy(() => import('./pages/examples/Legend'));
+const ExampleLogarithmicPriceScale = React.lazy(() => import('./pages/examples/LogarithmicPriceScale'));
+const ExampleMagnifierTooltip = React.lazy(() => import('./pages/examples/MagnifierTooltip'));
+const ExampleMovingAverage = React.lazy(() => import('./pages/examples/MovingAverage'));
+const ExampleNoPriceScale = React.lazy(() => import('./pages/examples/NoPriceScale'));
+const ExampleNoTimeScale = React.lazy(() => import('./pages/examples/NoTimeScale'));
+const ExamplePercentagePriceScale = React.lazy(() => import('./pages/examples/PercentagePriceScale'));
+const ExamplePriceLinesWithTitles = React.lazy(() => import('./pages/examples/PriceLinesWithTitles'));
+const ExamplePriceScaleAtLeft = React.lazy(() => import('./pages/examples/PriceScaleAtLeft'));
+const ExampleRangeSwitcher = React.lazy(() => import('./pages/examples/RangeSwitcher'));
+const ExampleRealtimeEmulation = React.lazy(() => import('./pages/examples/RealtimeEmulation'));
+const ExampleSeriesMarkers = React.lazy(() => import('./pages/examples/SeriesMarkers'));
+const ExampleThreeLineLegend = React.lazy(() => import('./pages/examples/ThreeLineLegend'));
+const ExampleTickMarksFormatter = React.lazy(() => import('./pages/examples/TickMarksFormatter'));
+const ExampleTrackingTooltip = React.lazy(() => import('./pages/examples/TrackingTooltip'));
+const ExampleTwoPriceScales = React.lazy(() => import('./pages/examples/TwoPriceScales'));
+const ExampleVolumeStudy = React.lazy(() => import('./pages/examples/VolumeStudy'));
+const ExampleWhitespaces = React.lazy(() => import('./pages/examples/Whitespaces'));
 
 function App() {
   return (
@@ -26,51 +55,284 @@ function App() {
             <ButtonLink to="examples">Examples</ButtonLink>
           </Pane>
         </Pane>
-
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <SuspenseLoader>
-                <Home />
-              </SuspenseLoader>
-            }
-          />
-          <Route path="examples">
+        <Pane display="flex" flexDirection="column" overflowY="auto">
+          <Routes>
             <Route
-              path=""
+              path="/"
               element={
                 <SuspenseLoader>
-                  <ExamplesHome />
+                  <Home />
                 </SuspenseLoader>
               }
             />
-            <Route
-              path="bar-chart"
-              element={
-                <SuspenseLoader>
-                  <ExamplesBarChart />
-                </SuspenseLoader>
-              }
-            />
-            <Route
-              path="candlestick-chart"
-              element={
-                <SuspenseLoader>
-                  <ExamplesCandlestickChart />
-                </SuspenseLoader>
-              }
-            />
-            <Route
-              path="custom-font-family"
-              element={
-                <SuspenseLoader>
-                  <ExamplesCustomFontFamily />
-                </SuspenseLoader>
-              }
-            />
-          </Route>
-        </Routes>
+            <Route path="examples">
+              <Route
+                path=""
+                element={
+                  <SuspenseLoader>
+                    <ExamplesHome />
+                  </SuspenseLoader>
+                }
+              />
+              <Route
+                path="bar-chart"
+                element={
+                  <SuspenseLoader>
+                    <ExampleBarChart />
+                  </SuspenseLoader>
+                }
+              />
+              <Route
+                path="candlestick-chart"
+                element={
+                  <SuspenseLoader>
+                    <ExampleCandlestickChart />
+                  </SuspenseLoader>
+                }
+              />
+              <Route
+                path="change-series-visibility"
+                element={
+                  <SuspenseLoader>
+                    <ExampleChangeSeriesVisibility />
+                  </SuspenseLoader>
+                }
+              />
+              <Route
+                path="custom-font-family"
+                element={
+                  <SuspenseLoader>
+                    <ExampleCustomFontFamily />
+                  </SuspenseLoader>
+                }
+              />
+              <Route
+                path="custom-locale"
+                element={
+                  <SuspenseLoader>
+                    <ExampleCustomLocale />
+                  </SuspenseLoader>
+                }
+              />
+              <Route
+                path="custom-price-formatter"
+                element={
+                  <SuspenseLoader>
+                    <ExampleCustomPriceFormatter />
+                  </SuspenseLoader>
+                }
+              />
+              <Route
+                path="custom-themes"
+                element={
+                  <SuspenseLoader>
+                    <ExampleCustomThemes />
+                  </SuspenseLoader>
+                }
+              />
+              <Route
+                path="custom-watermark"
+                element={
+                  <SuspenseLoader>
+                    <ExampleCustomWatermark />
+                  </SuspenseLoader>
+                }
+              />
+              <Route
+                path="fit-content"
+                element={
+                  <SuspenseLoader>
+                    <ExampleFitContent />
+                  </SuspenseLoader>
+                }
+              />
+              <Route
+                path="floating-tooltip"
+                element={
+                  <SuspenseLoader>
+                    <ExampleFloatingTooltip />
+                  </SuspenseLoader>
+                }
+              />
+              <Route
+                path="go-to-realtime-button"
+                element={
+                  <SuspenseLoader>
+                    <ExampleGoToRealtimeButton />
+                  </SuspenseLoader>
+                }
+              />
+              <Route
+                path="infinite-history"
+                element={
+                  <SuspenseLoader>
+                    <ExampleInfiniteHistory />
+                  </SuspenseLoader>
+                }
+              />
+              <Route
+                path="intraday-data"
+                element={
+                  <SuspenseLoader>
+                    <ExampleIntradayData />
+                  </SuspenseLoader>
+                }
+              />
+              <Route
+                path="inverted-price-scale"
+                element={
+                  <SuspenseLoader>
+                    <ExampleInvertedPriceScale />
+                  </SuspenseLoader>
+                }
+              />
+              <Route
+                path="legend"
+                element={
+                  <SuspenseLoader>
+                    <ExampleLegend />
+                  </SuspenseLoader>
+                }
+              />
+              <Route
+                path="logarithmic-price-scale"
+                element={
+                  <SuspenseLoader>
+                    <ExampleLogarithmicPriceScale />
+                  </SuspenseLoader>
+                }
+              />
+              <Route
+                path="magnifier-tooltip"
+                element={
+                  <SuspenseLoader>
+                    <ExampleMagnifierTooltip />
+                  </SuspenseLoader>
+                }
+              />
+              <Route
+                path="moving-average"
+                element={
+                  <SuspenseLoader>
+                    <ExampleMovingAverage />
+                  </SuspenseLoader>
+                }
+              />
+              <Route
+                path="no-price-scale"
+                element={
+                  <SuspenseLoader>
+                    <ExampleNoPriceScale />
+                  </SuspenseLoader>
+                }
+              />
+              <Route
+                path="no-time-scale"
+                element={
+                  <SuspenseLoader>
+                    <ExampleNoTimeScale />
+                  </SuspenseLoader>
+                }
+              />
+              <Route
+                path="percentage-price-scale"
+                element={
+                  <SuspenseLoader>
+                    <ExamplePercentagePriceScale />
+                  </SuspenseLoader>
+                }
+              />
+              <Route
+                path="price-lines-with-titles"
+                element={
+                  <SuspenseLoader>
+                    <ExamplePriceLinesWithTitles />
+                  </SuspenseLoader>
+                }
+              />
+              <Route
+                path="price-scale-at-left"
+                element={
+                  <SuspenseLoader>
+                    <ExamplePriceScaleAtLeft />
+                  </SuspenseLoader>
+                }
+              />
+              <Route
+                path="range-switcher"
+                element={
+                  <SuspenseLoader>
+                    <ExampleRangeSwitcher />
+                  </SuspenseLoader>
+                }
+              />
+              <Route
+                path="realtime-emulation"
+                element={
+                  <SuspenseLoader>
+                    <ExampleRealtimeEmulation />
+                  </SuspenseLoader>
+                }
+              />
+              <Route
+                path="series-markers"
+                element={
+                  <SuspenseLoader>
+                    <ExampleSeriesMarkers />
+                  </SuspenseLoader>
+                }
+              />
+              <Route
+                path="three-line-legend"
+                element={
+                  <SuspenseLoader>
+                    <ExampleThreeLineLegend />
+                  </SuspenseLoader>
+                }
+              />
+              <Route
+                path="tick-marks-formatter"
+                element={
+                  <SuspenseLoader>
+                    <ExampleTickMarksFormatter />
+                  </SuspenseLoader>
+                }
+              />
+              <Route
+                path="tracking-tooltip"
+                element={
+                  <SuspenseLoader>
+                    <ExampleTrackingTooltip />
+                  </SuspenseLoader>
+                }
+              />
+              <Route
+                path="two-price-scales"
+                element={
+                  <SuspenseLoader>
+                    <ExampleTwoPriceScales />
+                  </SuspenseLoader>
+                }
+              />
+              <Route
+                path="volume-study"
+                element={
+                  <SuspenseLoader>
+                    <ExampleVolumeStudy />
+                  </SuspenseLoader>
+                }
+              />
+              <Route
+                path="whitespaces"
+                element={
+                  <SuspenseLoader>
+                    <ExampleWhitespaces />
+                  </SuspenseLoader>
+                }
+              />
+            </Route>
+          </Routes>
+        </Pane>
       </Pane>
     </BrowserRouter>
   );
--- demo/src/pages/examples/Home.tsx
+++ demo/src/pages/examples/Home.tsx
@@ -13,13 +13,16 @@ const Home: React.FC = () => {
         <Link to="candlestick-chart">Candlestick Chart</Link>
       </ListItem>
       <ListItem>
+        <Link to="change-series-visibility">Change Series Visibility</Link>
+      </ListItem>
+      <ListItem>
         <Link to="custom-font-family">Custom Font Family</Link>
       </ListItem>
       <ListItem>
-        <Link to="custom-price-formatter">Custom Price Formatter</Link>
+        <Link to="custom-locale">Custom Locale</Link>
       </ListItem>
       <ListItem>
-        <Link to="custom-locale">Custom Locale</Link>
+        <Link to="custom-price-formatter">Custom Price Formatter</Link>
       </ListItem>
       <ListItem>
         <Link to="custom-themes">Custom Themes</Link>
@@ -28,22 +31,76 @@ const Home: React.FC = () => {
         <Link to="custom-watermark">Custom Watermark</Link>
       </ListItem>
       <ListItem>
-        <Link to="legend">Legend</Link>
-      </ListItem>
-      <ListItem>
-        <Link to="3-line-legend">3-Line Legend</Link>
-      </ListItem>
-      <ListItem>
         <Link to="fit-content">Fit Content</Link>
       </ListItem>
       <ListItem>
         <Link to="floating-tooltip">Floating Tooltip</Link>
       </ListItem>
       <ListItem>
-        <Link to="tracking-tooltip">Tracking Tooltip</Link>
+        <Link to="go-to-realtime-button">Go To Realtime Button</Link>
+      </ListItem>
+      <ListItem>
+        <Link to="infinite-history">Infinite History</Link>
+      </ListItem>
+      <ListItem>
+        <Link to="intraday-data">Intraday Data</Link>
+      </ListItem>
+      <ListItem>
+        <Link to="inverted-price-scale">Inverted Price Scale</Link>
+      </ListItem>
+      <ListItem>
+        <Link to="legend">Legend</Link>
+      </ListItem>
+      <ListItem>
+        <Link to="logarithmic-price-scale">Logarithmic Price Scale</Link>
       </ListItem>
       <ListItem>
         <Link to="magnifier-tooltip">Magnifier Tooltip</Link>
+      </ListItem>
+      <ListItem>
+        <Link to="moving-average">Moving Average</Link>
+      </ListItem>
+      <ListItem>
+        <Link to="no-price-scale">No Price Scale</Link>
+      </ListItem>
+      <ListItem>
+        <Link to="no-time-scale">No Time Scale</Link>
+      </ListItem>
+      <ListItem>
+        <Link to="percentage-price-scale">Percentage Price Scale</Link>
+      </ListItem>
+      <ListItem>
+        <Link to="price-lines-with-titles">Price Lines With Titles</Link>
+      </ListItem>
+      <ListItem>
+        <Link to="price-scale-at-left">Price Scale At Left</Link>
+      </ListItem>
+      <ListItem>
+        <Link to="range-switcher">Range Switcher</Link>
+      </ListItem>
+      <ListItem>
+        <Link to="realtime-emulation">Realtime Emulation</Link>
+      </ListItem>
+      <ListItem>
+        <Link to="series-markers">Series Markers</Link>
+      </ListItem>
+      <ListItem>
+        <Link to="three-line-legend">Three Line Legend</Link>
+      </ListItem>
+      <ListItem>
+        <Link to="tick-marks-formatter">Tick Marks Formatter</Link>
+      </ListItem>
+      <ListItem>
+        <Link to="tracking-tooltip">Tracking Tooltip</Link>
+      </ListItem>
+      <ListItem>
+        <Link to="two-price-scales">Two Price Scales</Link>
+      </ListItem>
+      <ListItem>
+        <Link to="volume-study">Volume Study</Link>
+      </ListItem>
+      <ListItem>
+        <Link to="whitespaces">Whitespaces</Link>
       </ListItem>
     </UnorderedList>
   );
